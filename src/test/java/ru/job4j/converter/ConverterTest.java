74bf4424c9fe4af86c5edea5cc23b634b7dec844
GrnDev1 <grndev@mail.ru>
2022-11-23 20:39:05 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+     float in = 140;
+     float expected = 2;
+     float out = Converter.rubleToEuro(in);
+     float eps = 0.1f;
+     Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert200RblThen3dot3333Dollar() {
+        float in = 200;
+        float expected = 3.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert2EuroThen140Rbl() {
+        float in = 2;
+        float expected = 140;
+        float out = Converter.euroToRuble(in);
+        float eps = 0.1f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert2DollarThen120Rbl() {
+        float in = 2;
+        float expected = 120;
+        float out = Converter.dollarToRuble(in);
+        float eps = 0.1f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert100DollarThen86Euro() {
+        float in = 100;
+        float expected = 86;
+        float out = Converter.dollarToEuro(in);
+        float eps = 0.1f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert86EuroThen100Dollar() {
+        float in = 86;
+        float expected = 100;
+        float out = Converter.euroToDollar(in);
+        float eps = 0.3f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
