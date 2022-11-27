210b6baf86df90a253c84b4a83a271f0478e5680
GrnDev1 <grndev@mail.ru>
2022-11-27 19:17:40 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Roman";
+        names[1] = "Petr";
+        names[2] = "Egor";
+        names[3] = "Pavel";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
