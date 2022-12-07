b51449fa3623c117b7129e9b5df9536b8dcb4757
GrnDev1 <grndev@mail.ru>
2022-12-07 17:47:17 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,51 +1,39 @@
 package ru.job4j.condition;
 
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
 
     @Test
     public void when00To20Then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 
     @Test
     public void when56To99Then5() {
+        Point a = new Point(5, 6);
+        Point b = new Point(9, 9);
         double expected = 5;
-        int x1 = 5;
-        int y1 = 6;
-        int x2 = 9;
-        int y2 = 9;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 
     @Test
     public void when00To43Then5() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 3);
         double expected = 5;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 4;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 
     @Test
     public void when00To03Then3() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 3);
         double expected = 3;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 }
